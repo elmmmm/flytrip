82f7670a6915294c99b778b9625a65683c49e731
elmmmm <itsukiyr@gmail.com>
2023-08-11 18:55:39 +0800

    new dist

---
+++ docs/assets/city-f7418a83.js
@@ -0,0 +1,1 @@
+import{a5 as e}from"./index-1b93f14d.js";import{h as i}from"./index-f28b6d1e.js";function a(){return i.get({url:"/city/all"})}const l=e("city",{state:()=>({allCities:{},currentCity:{cityName:"广州"}}),actions:{async fetchAllCitiesData(){const t=await a();this.allCities=t.data}}});export{l as u};
---
+++ docs/assets/use-id-64ea88e5.js
@@ -0,0 +1,1 @@
+import{E as e}from"./index-1b93f14d.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
